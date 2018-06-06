44eb2ae40792c890260011c0c1f657e87ccbb42e
UvgenGen <sagirov19@gmail.com>
2018-06-06 12:27:28 +0300

    upgrade person_list

--- person/models.py
+++ person/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class person(models.Model):
 	name = models.CharField(max_length=255)
 	surname = models.CharField(max_length=255)
-	date_of_birth = models.CharField(max_length=255)
+	date_of_birth = models.DateField(max_length=255)
 	email = models.EmailField()
 	phone = models.CharField(max_length=255)
 
